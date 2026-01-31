1406380ed4635493fafe2c9264c2a0d316a88f5c
skyfortsec <itjoeldazz@gmail.com>
2026-01-31 13:30:09 +0000

    Update variables.tf

--- infra/platform/variables.tf
+++ infra/platform/variables.tf
@@ -1,8 +1,22 @@
-variable "region" { type = string, default = "eu-west-2" }
-variable "project" { type = string, default = "cloud4future" }
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
 
-variable "cluster_name" { type = string, default = "cloud4future-eks" }
-variable "vpc_cidr" { type = string, default = "10.20.0.0/16" }
+variable "project" {
+  type    = string
+  default = "cloud4future"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "cloud4future-eks"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.20.0.0/16"
+}
 
 variable "my_public_ip_cidr" {
   type        = string
