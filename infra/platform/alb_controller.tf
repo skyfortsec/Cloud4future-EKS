b0599ce117223c1e18a337655cb26ac72e1a9962
skyfortsec <itjoeldazz@gmail.com>
2026-01-31 13:30:42 +0000

    Update alb_controller.tf

--- infra/platform/alb_controller.tf
+++ infra/platform/alb_controller.tf
@@ -22,15 +22,36 @@ resource "helm_release" "aws_load_balancer_controller" {
   chart      = "aws-load-balancer-controller"
   version    = "1.7.2"
 
-  set { name = "clusterName" value = module.eks.cluster_name }
-  set { name = "serviceAccount.create" value = "true" }
-  set { name = "serviceAccount.name" value = "aws-load-balancer-controller" }
   set {
-    name  = "serviceAccount.annotations.eks\.amazonaws\.com/role-arn"
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  # IMPORTANT: double-escape for Terraform string
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = module.lb_controller_irsa.iam_role_arn
   }
-  set { name = "region" value = var.region }
-  set { name = "vpcId" value = module.vpc.vpc_id }
+
+  set {
+    name  = "region"
+    value = var.region
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
 
   depends_on = [module.eks, module.lb_controller_irsa]
 }
