8c5037a3cb119a0c902f2c8f3a6a84285130f739
skyfortsec <itjoeldazz@gmail.com>
2026-01-31 13:37:18 +0000

    Update providers.tf

--- infra/platform/providers.tf
+++ infra/platform/providers.tf
@@ -17,16 +17,8 @@ data "aws_eks_cluster_auth" "this" {
   depends_on = [module.eks]
 }
 
-# Kubernetes provider
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
-}
-
-# Helm provider (NO nested kubernetes {} block)
-provider "helm" {
-  kubernetes_host                   = data.aws_eks_cluster.this.endpoint
-  kubernetes_cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  kubernetes_token                  = data.aws_eks_cluster_auth.this.token
 }
